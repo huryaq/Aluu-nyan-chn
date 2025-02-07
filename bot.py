e2b2bfa4d500ebea6163196696a6ef73674e8a53
huryaq <hurryapurrya@gmail.com>
2025-02-07 19:46:00 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Amex_Fushiguro")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Save_ios")
         self.LOGGER(__name__).info(f""" \n\n       
 ░█████╗░███╗░░██╗██╗███╗░░░███╗███████╗██╗░░██╗██╗░░░██╗███╗░░██╗████████╗███████╗██████╗░░██████╗
 ██╔══██╗████╗░██║██║████╗░████║██╔════╝██║░░██║██║░░░██║████╗░██║╚══██╔══╝██╔════╝██╔══██╗██╔════╝
