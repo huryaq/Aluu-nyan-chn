19f0d58f3ea126a4b7927d299f1c86f145e60460
huryaq <hurryapurrya@gmail.com>
2025-02-16 13:43:18 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "AlyaFilesBot")
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1002360962434"))
-FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1001715114844"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002292347562"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
